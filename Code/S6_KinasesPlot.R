b776be0445fb62766522346412869ee20f7972e2
Daniel Osorio <dcosorioh@tamu.edu>
2021-09-02 15:51:45 -0500

    Create S6_KinasesPlot.R

---
+++ Code/S6_KinasesPlot.R
@@ -0,0 +1,12 @@
+load('../Data/EpithelialCells.RData')
+library(Nebulosa)
+
+P <- plot_density(breastData[,breastData$diseaseStatus == 'Cancer'], c('BTK','BMX', 'TEC'), joint = TRUE, method = 'ks')
+P[[1]] <- P[[1]] + theme_bw() + theme(legend.position = 'none', plot.title = element_text(face = 2))
+P[[2]] <- P[[2]] + theme_bw() + theme(legend.position = 'none', plot.title = element_text(face = 2))
+P[[3]] <- P[[3]] + theme_bw() + theme(legend.position = 'none', plot.title = element_text(face = 2))
+P[[4]] <- P[[4]] + theme_bw() + theme(legend.position = 'none', plot.title = element_text(face = 2))
+
+png('../Figures/S3.png', width = 2000, height = 2000, res = 300)
+P
+dev.off()
